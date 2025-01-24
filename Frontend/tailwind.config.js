6174011a8b9781b1caf17c739e0a2284c5015eba
SABRY225 <ahmed010144497@gmail.com>
2025-01-24 18:50:29 +0200

    add colors

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,7 +4,24 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor:"#6C63FF",
+        cardColor:"#E5E5FF",
+        backLigth:"#F5F5FF",
+        secondColor:"#FF6F61",
+        red:"#FF0000",
+        green:"#3DCF3D",
+        balck:"#000000",
+        button:"#FFA563"
+      },
+      fontFamily:{
+        Font1:["Roboto Condensed"],
+        Font2:["Namdhinggo"],
+        Font3:["Chivo"],
+        Font4:["Markazi Text"]
+      }
+    },
   },
   plugins: [],
 }
